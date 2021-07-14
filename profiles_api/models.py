b1b2b7860e9a75986113a946b4e34ea1e76bc8ce
frostzt <aidenfrostbite@gmail.com>
2021-07-14 21:09:53 +0530

    creating UserModels

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database models for Users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of the user"""
+        return self.name
